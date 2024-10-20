a28d199466a98a818f56579f7f434f1c0598d4d1
Darius Moldovan <darius226612@gmail.com>
2024-10-20 22:51:50 +0300

    Done

---
+++ Data/Moldovan_Darius_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Darius_Lab2.Models;
+
+namespace Moldovan_Darius_Lab2.Data
+{
+    public class Moldovan_Darius_Lab2Context : DbContext
+    {
+        public Moldovan_Darius_Lab2Context (DbContextOptions<Moldovan_Darius_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moldovan_Darius_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Moldovan_Darius_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Moldovan_Darius_Lab2.Models.Author> Authors { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Darius_Lab2.Data;
+using Moldovan_Darius_Lab2.Models;
+
+namespace Moldovan_Darius_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Moldovan_Darius_Lab2.Data.Moldovan_Darius_Lab2Context _context;
+
+        public IndexModel(Moldovan_Darius_Lab2.Data.Moldovan_Darius_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b=>b.Publisher)
+                .Include(b=>b.Author)
+                .ToListAsync();
+        }
+    }
+}
